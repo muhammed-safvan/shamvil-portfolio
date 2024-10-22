3a5f3b4c94054d39e54486bdea9d915adcb81bd3
muhammed-safvan <mamuhammedsafvan617@gmail.com>
2024-10-22 22:59:11 +0530

    added projects container styles

--- scripts.js
+++ scripts.js
@@ -3,8 +3,20 @@ import { openCloseMenu } from "./scripts/display-change.js";
 
 import { useSmoothScroll } from "./scripts/scroll-effect.js";
 
+import { enableHorizontalScroll } from "./scripts/scroll-effect.js";
+
+const projectInfoWraps = document.querySelectorAll('.project-info-wrap');
+
+const projectsContainer = document.querySelector('.projects-container');
+
 openCloseMenu();
 useSmoothScroll();
+
+
+projectInfoWraps.forEach((wrap) => {
+    enableHorizontalScroll(wrap);
+    useSmoothScroll(projectsContainer,wrap)
+})
 
 
 
--- scripts/scroll-effect.js
+++ scripts/scroll-effect.js
@@ -2,15 +2,23 @@ const menuContainer = document.querySelector("#menu-container");
 
 const menuLinkWrap = document.querySelector(".menu-links-wrapper");
 
+
+
 export function useSmoothScroll(parent = menuContainer, child = menuLinkWrap) {
   parent.addEventListener("scroll", () => {
     const scrollTop = parent.scrollTop - 65;
-    const maxScrollTop =
-      parent.scrollHeight - parent.clientHeight;
+    const maxScrollTop = parent.scrollHeight - parent.clientHeight;
     const scrollFraction = scrollTop / maxScrollTop;
 
-    child.style.transform = `translateY(-${
-      scrollFraction * 100
-    }%) scaleY(${1 + scrollFraction * 0.05})`;
+    child.style.transform = `translateY(-${scrollFraction * 100}%) scaleY(${
+      1 + scrollFraction * 0.05
+    })`;
+  });
+}
+
+export function enableHorizontalScroll(container) {
+  container.addEventListener("wheel", (e) => {
+    e.preventDefault(); // Prevent default scrolling behavior
+    container.scrollLeft += e.deltaY; // Scroll horizontally
   });
 }
